bb54034c9a98dfc7fc96e6e45fd83f558f8956e0
To Viet <tvthang@titops.com>
2021-11-08 15:02:58 +0700

    Handle like someone

--- frontend/src/containers/users/index.js
+++ frontend/src/containers/users/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from "react-redux";
 import {loadUsers} from "./actions"
 import {bindActionCreators} from "redux"
+import {notify} from "react-notify-toast"
 import './style.scss';
 
 class Index extends React.Component {
@@ -25,19 +26,43 @@ class Index extends React.Component {
 		super(props);
 		this.state = {
 			i: 0,
+			isShow: false
 		};
 	}
 
 	_handleNextOne = async (i, limit, page) => {
 		if (i < limit - 1){
-			this.setState({
-				i: i + 1
-			});
+			setTimeout(() => {
+				this.setState({
+					i: i + 1
+				});
+			}, 1200);
+
 		}else {
 			await this.props.loadUsers(limit,page + 1);
 			this.setState({
 				i: 0
 			});
+		}
+	};
+
+	_handleLike = async (i, limit, page, user) => {
+		notify.show(`You Like ${user.firstName}`, 'success',1000);
+		this._handleNextOne(i, limit, page);
+		let arrLiked = localStorage.getItem('arrLiked');
+		//localStorage.setItem('arrLiked', JSON.stringify(user));
+	};
+
+	_handleSkip = async (i, limit, page) => {
+		if (i < limit - 1){
+			this.setState({
+				i: i + 1
+			});
+		}else {
+			this.setState({
+				i: 0
+			});
+			await this.props.loadUsers(limit,page + 1);
 		}
 	};
 
@@ -54,7 +79,7 @@ class Index extends React.Component {
 
 	render() {
 		const {error, loaded, list, currentPage, limit} = this.props.users;
-		const {i} = this.state;
+		const {i} = this.state || 0;
 		const user = list[i];
 		return (
 			loaded && !error ? (
@@ -68,10 +93,32 @@ class Index extends React.Component {
 						</h3>
 					</div>
 					<div className="group-btn">
-						<a className="label label-danger"
+						<a className="label"
 						   onClick={(e) => this._handleNextOne(i, limit, currentPage)}>
-							Like
+							<img src="https://cdn-icons-png.flaticon.com/512/458/458594.png"/>
 						</a>
+						<a className="label"
+						   onClick={(e) => this._handleLike(i, limit, currentPage, user)}>
+							<img src="https://findicons.com/files/icons/734/phuzion/128/fav_heart.png"/>
+						</a>
+					</div>
+					<div className="group-action">
+						<div>
+							<a>
+								Like
+							</a>
+
+						</div>
+						<div>
+							<a>
+								Discover
+							</a>
+						</div>
+						<div>
+							<a>
+								Match
+							</a>
+						</div>
 					</div>
 
 				</div>
